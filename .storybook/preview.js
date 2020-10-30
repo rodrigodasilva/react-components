225a26969b677081ed25d9ce79fe6bdfca74363d
rodrigodasilva <ribeiro.rodrigodasilva>
2020-10-30 18:05:53 -0300

    doc: adiçao de arquivo css para documentaçao

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { addDecorator } from '@storybook/react'
+import React from 'react';
+import { addDecorator } from '@storybook/react';
 
 import GlobalStyle from '../src/styles/global';
-import {ContainerToast} from '../src/components/CustomToast'
+import {ContainerToast} from '../src/components/CustomToast';
+
+import './styles/storybook.css'
 
 addDecorator(storyFn =>
   <>
